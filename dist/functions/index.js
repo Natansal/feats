655b5d69f27c8d5e1607a0c37385730a7f6513b0
Natansal-weme <natan@weme.community>
2025-07-30 15:49:37 +0300

    Delete dist directory

--- dist/functions/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.text = void 0;
-const text_1 = require("./text");
-Object.defineProperty(exports, "text", { enumerable: true, get: function () { return text_1.text; } });
