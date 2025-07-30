3021133f959338c9e65a7e75d0203e61fc27b031
Natan Salmon <Natan@weme.community>
2025-07-30 17:12:33 +0300

    rollup conig change

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,33 @@
 import typescript from "@rollup/plugin-typescript";
 
+// Define the formats you want to build
+const formats = [
+   { name: "cjs", ext: ".cjs" },
+   { name: "esm", ext: ".mjs" },
+];
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
 export default {
    input: {
       index: "src/index.ts",
       internals: "src/internals.ts",
    },
-   output: {
+
+   // Dynamically generate the output array
+   output: formats.map((format) => ({
       dir: "dist",
-      format: "es",
+      format: format.name,
+      entryFileNames: `[name]${format.ext}`,
       sourcemap: true,
-   },
-   plugins: [typescript()],
+   })),
+
+   plugins: [
+      typescript({
+         target: "ES2018",
+         declaration: true,
+         declarationDir: "dist",
+      }),
+   ],
 };
